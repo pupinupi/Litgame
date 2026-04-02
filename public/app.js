ff937285500ecf1ba7f04f3fb1b913d2490cedf5
pupinupi <cool.lg-lg@yandex.ru>
2026-04-02 09:37:25 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 // --- ЗВУКИ ---
 const scandalSound = new Audio('scandal.mp3');
-scandalSound.volume = 0.8;
+scandalSound.volume = 0.6;
 
 const diceSound = new Audio('dice.mp3');
-diceSound.volume = 0.9;
+diceSound.volume = 1.3;
 
 const socket = io();
 
